430a478e983508de5137a315c9b6c28fb960e0eb
Gerard Montilla Romia <gerardmr90@gmail.com>
2017-11-20 13:07:12 +0000

    Passing CompanyExtractor test

--- src/CompanyExtractor.php
+++ src/CompanyExtractor.php
@@ -3,5 +3,8 @@ final class CompanyExtractor
 {
     public static function getCompanyFromEmailAddress($email)
     {
+        $domain = substr(strstr($email, '@'), 1);
+        $company = substr(strstr($domain, '.', true), 0);
+        return $company;
     }
 }
