25f1beb92bbc5cfe85ed9086f40b0b2296cfb283
Gerard Montilla Romia <gerardmr90@gmail.com>
2017-11-21 23:05:49 +0000

    Passing WebScrapper getPhoneNumberFromWebsite test

--- src/WebScrapper.php
+++ src/WebScrapper.php
@@ -2,6 +2,7 @@
 
 const EMAIL_REGEX = '/[a-z\d._%+-]+@[a-z\d.-]+\.[a-z]{2,4}\b/i';
 const POSTCODE_REGEX = '/[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}/';
+const PHONE_REGEX = '/\+?\d{1,2} \d{2,4} \d{2,4} \d{2,4}/';
 
 final class WebScrapper
 {
@@ -14,9 +15,8 @@ final class WebScrapper
         if ($matches) {
             $emails = array_unique($matches[0]);
             return implode(',', $emails);
-        } else {
-            return 'Email address not found';
         }
+        return 'Email address not found';
     }
 
     public static function getPostCodeFromWebsite($url)
@@ -28,12 +28,20 @@ final class WebScrapper
         if ($matches) {
             $postcodes = array_unique($matches[0]);
             return implode(',', $postcodes);
-        } else {
-            return 'Postcode not found';
         }
+        return 'Postcode not found';
     }
 
     public static function getPhoneNumberFromWebsite($url)
     {
+        $html = file_get_contents($url);
+
+        preg_match_all(PHONE_REGEX, $html, $matches);
+
+        if ($matches) {
+            $phones = array_unique($matches[0]);
+            return implode(',', $phones);
+        }
+        return 'Phone Number not found';
     }
 }
