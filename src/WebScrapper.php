1d4e9b8e6b3953f268f19b052211274c8090c1e5
Gerard Montilla Romia <gerardmr90@gmail.com>
2017-11-21 14:58:07 +0000

    Passing WebScrapper getEmailAddressFromWebsite test

--- src/WebScrapper.php
+++ src/WebScrapper.php
@@ -1,8 +1,20 @@
 <?php
 
+const EMAIL_REGEX = '/[a-z\d._%+-]+@[a-z\d.-]+\.[a-z]{2,4}\b/i';
+
 final class WebScrapper
 {
     public static function getEmailAddressFromWebsite($url)
     {
+        $html = file_get_contents($url);
+
+        preg_match_all(EMAIL_REGEX, $html, $matches);
+
+        if ($matches) {
+            $emails = array_unique($matches[0]);
+            return implode(',', $emails);
+        } else {
+            return 'Email address not found';
+        }
     }
 }
