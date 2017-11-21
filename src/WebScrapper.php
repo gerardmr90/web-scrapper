5dfd3e2a36d16721fbf1a960af08429fd0ca1b55
Gerard Montilla Romia <gerardmr90@gmail.com>
2017-11-21 20:56:22 +0000

    Passing WebScrapper getPostCodeFromWebsite test

--- src/WebScrapper.php
+++ src/WebScrapper.php
@@ -1,6 +1,7 @@
 <?php
 
 const EMAIL_REGEX = '/[a-z\d._%+-]+@[a-z\d.-]+\.[a-z]{2,4}\b/i';
+const POSTCODE_REGEX = '/[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}/';
 
 final class WebScrapper
 {
@@ -20,5 +21,15 @@ final class WebScrapper
 
     public static function getPostCodeFromWebsite($url)
     {
+        $html = file_get_contents($url);
+
+        preg_match_all(POSTCODE_REGEX, $html, $matches);
+
+        if ($matches) {
+            $postcodes = array_unique($matches[0]);
+            return implode(',', $postcodes);
+        } else {
+            return 'Postcode not found';
+        }
     }
 }
